1c32a30745529ea39d7d743957b1875b93959a14
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2023-10-29 19:52:25 +0600

    Logic

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Logistics roadLogistics = new RoadLogistics();
+        Logistics seaLogistics = new SeaLogistics();
+        Logistics skyLogistics = new SkyLogistics();
+
+        String cargo = "00B201";
+        String destination = "New York";
+
+        Transport transport = roadLogistics.createTransport();
+        transport.deliver(cargo, destination);
+
+        Order order = new Order();
+        DeliveryObserver customer = new Customer("Jack");
+        order.addCustomer(customer);
+
+        order.setStatus("Shipped");
+        order.setStatus("In Transit");
+        order.setStatus("At your house");
     }
 }
