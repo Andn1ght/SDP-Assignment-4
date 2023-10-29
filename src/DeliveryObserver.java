9b2dbbee60abc2d18419b8352bb9f482d239bb4c
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2023-10-29 19:58:30 +0600

    JavDoc

--- src/DeliveryObserver.java
+++ src/DeliveryObserver.java
@@ -1,3 +1,11 @@
+/**
+ * The DeliveryObserver interface defines a contract for objects that can receive and respond to status updates about deliveries.
+ */
 public interface DeliveryObserver {
+    /**
+     * Update the observer with a status message about a delivery.
+     *
+     * @param status The status message to be received and processed by the observer.
+     */
     void update(String status);
 }
